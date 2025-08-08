7c29a984ce4da1353db9618954ce3e29d2d8283b
moritz-strobel <moritz.strobel@icloud.com>
2025-08-08 15:55:04 +0200

    task: lock menu pages in portrait mode

--- rc-car-maui-app/AppShell.xaml.cs
+++ rc-car-maui-app/AppShell.xaml.cs
@@ -1,13 +1,22 @@
-﻿using rc_car_maui_app.Views;
+﻿using rc_car_maui_app.Services;
+using rc_car_maui_app.Views;
 
 namespace rc_car_maui_app;
 
 public partial class AppShell : Shell
 {
+    private readonly IDeviceOrientationService _orientationService;
     public AppShell()
     {
         InitializeComponent();
         CurrentItem = HomeTab;
         Routing.RegisterRoute(nameof(ManualDrivingPage), typeof(ManualDrivingPage));
+        _orientationService = new DeviceOrientationService();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        _orientationService.SetPortrait();
     }
 }
