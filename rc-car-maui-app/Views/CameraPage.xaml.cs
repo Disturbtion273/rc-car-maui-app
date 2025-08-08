c59f0095d8f558ddd016773d622dfcf83d5aa845
Florian Höck <florian.hoeck4@gmail.com>
2025-08-08 19:01:24 +0200

    Create CameraPage.xaml, CameraPage.xaml.cs

---
+++ rc-car-maui-app/Views/CameraPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace rc_car_maui_app.Views;
+
+public partial class CameraPage : ContentPage
+{
+    public CameraPage()
+    {
+        InitializeComponent();
+    }
+    
+    private async void OnHomeClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("///MainPage");
+    }
+
+
+}
