4afdf6ce1c476a7e011a8a6d6389eab99cfbdf05
Thanachot <thanachot.mankong@mercedes-benz.com>
2025-08-11 02:42:48 +0200

    Arrange the basic structure of the card contents

--- rc-car-maui-app/Views/DrivingPage.xaml.cs
+++ rc-car-maui-app/Views/DrivingPage.xaml.cs
@@ -6,4 +6,21 @@ public partial class DrivingPage : ContentPage
     {
         InitializeComponent();
     }
+    
+    // these function serve as placeholder for now
+    // for later these function should navigate to the corrected driving mode view
+    private void DrivingOne(object sender, EventArgs e)
+    {
+        Console.WriteLine("First Driving Mode");
+    }
+    
+    private void DrivingTwo(object sender, EventArgs e)
+    {
+        Console.WriteLine("Second Driving Mode");
+    }
+    
+    private void DrivingThree(object sender, EventArgs e)
+    {
+        Console.WriteLine("Third Driving Mode");
+    }
 }
