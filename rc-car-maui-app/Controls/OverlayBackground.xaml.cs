287dde7ddaa1221947ffd94ab26b229eb473a238
LSENTEN <luca.sentenstein@mercedes-benz.com>
2025-08-18 08:23:32 +0200

    feat: add OverlayBackground control for UI overlay

---
+++ rc-car-maui-app/Controls/OverlayBackground.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rc_car_maui_app.Controls;
+
+public partial class OverlayBackground : Border
+{
+    public OverlayBackground()
+    {
+        InitializeComponent();
+    }
+}
