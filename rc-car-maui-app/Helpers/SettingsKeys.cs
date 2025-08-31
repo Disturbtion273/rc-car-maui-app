b3a8782b17d60374d40aed40ad4b214c104368c8
Florian Höck <florian.hoeck4@gmail.com>
2025-08-31 13:50:29 +0200

    add SettingsKeys constants for BatteryIndicator preference and messaging

---
+++ rc-car-maui-app/Helpers/SettingsKeys.cs
@@ -0,0 +1,8 @@
+namespace rc_car_maui_app.Helpers
+{
+    public static class SettingsKeys
+    {
+        public const string ShowBatteryIndicator = "ShowBatteryIndicator";
+        public const string ShowBatteryIndicatorChanged = "ShowBatteryIndicatorChanged"; 
+    }
+}
