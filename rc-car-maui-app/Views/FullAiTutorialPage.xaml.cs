5f40ebfe4cf24ded62614420ce1eb0533037e9e2
tmankon <mankongt.tin24@student.dhbw-heidenheim.de>
2025-09-16 10:05:25 +0200

    feat: add driving tutorial (#118)
    * Add tutorial icon next to driving modes title
    
    * Add tutorial pages navigation
    
    * Implement tutorial for manual driving
    
    * Finish up tutorial page
    
    * Add content to ful-ai tutorial
    
    * Fix Full-AI tutorial spacing

---
+++ rc-car-maui-app/Views/FullAiTutorialPage.xaml.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rc_car_maui_app.Views;
+
+public partial class FullAiTutorialPage : ContentPage
+{
+    public FullAiTutorialPage()
+    {
+        InitializeComponent();
+    }
+    
+    private async void BackButton(object sender, EventArgs e)
+    {
+        await Shell.Current.Navigation.PopAsync(animated: true);
+    }
+}
